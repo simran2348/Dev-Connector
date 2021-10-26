bd1aa6d420ac4f59f58e594886d56846d66d6da7
Simranjit Singh <theliongitlab@gmail.com>
2021-10-26 20:18:51 +0530

    added jwt token

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,8 @@ const connection = require('./config/connection')
 
 const app = express()
 
+const PORT = process.env.PORT || 5000
+
 //Connect Database
 connection()
 
@@ -19,8 +21,6 @@ app.use('/api/users', require('./routes/api/users'))
 app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/profile', require('./routes/api/profile'))
 app.use('/api/posts', require('./routes/api/posts'))
-
-const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`)
